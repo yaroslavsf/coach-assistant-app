fa1ca51192aaa16f79deb69cfcf3406cd8c0c0c9
yaroslavsf <yaroslav1598753@gmail.com>
2022-07-20 23:43:38 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+
+import {Navbar} from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <h1>Coach assistant</h1>
+      <h2>Regulation system of trainings</h2>
+      <hr/>
+      <Navbar>
+        <Routes>
+          {/* TODO */}
+        </Routes>
+      </Navbar>
+      </Router>
     </div>
+    
   );
 }
 
